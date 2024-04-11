9e95f6adb05b887eac3238806ddefcb42848d92b
Pradyoth Velagapudi <119096365+PradyothVelagapudi@users.noreply.github.com>
2024-04-10 21:49:44 -0700

    Update PatientLoginPage.java

--- src/application/PatientLoginPage.java
+++ src/application/PatientLoginPage.java
@@ -102,7 +102,8 @@ public class PatientLoginPage extends Application
         
         else
         {
-            errorLabel.setText("Invalid username or password.");
+            errorLabel.setText("Invalid login.");
+            // error message does not tell the user exactly what is incorrect-- prevents oversharing info
         }
     }
 
